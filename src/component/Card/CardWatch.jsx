766f93da7179f4651e0b387111828a1759d56b72
RezaN97 <rezanurit16@gmail.com>
2025-05-31 00:24:17 +0700

    update card section

--- src/component/Card/CardWatch.jsx
+++ src/component/Card/CardWatch.jsx
@@ -1,38 +1,57 @@
+ import arwLeft from '../../img/arrow-left.png'
+ import arwRight from '../../img/arrow-right.png'
+
 const CardWatch = () => {
     return (
         <>
-          <h2 className="judul-awal">Melanjutkan Tonton Film</h2>
-            <section className="slider-nonton">
-                <div id="image-slider">
-                    <div id="img-look">
-                        <div id="btn-arrow-left-sld1" />
-                        <div id="txt-nama-film">Don't Look Up
-                            <div id="txt-rate-film"> &#9733; 4.5/5</div>
+        <section className=" font-Lato flex flex-col top-[681px] md:w-[1440px] md:h-[309px] px-[80px] py-[40px] gap-[32px]">
+                <h2 className="text-white text-[32px] font-bold">Melanjutkan Tonton Film</h2>
+                <div id="image-slider" className="flex flex-row gap-[24px] text-white">
+                    <div id="img-look" className="">
+                        <div id="txt-nama-film" className="flex text bg-[url('../../img/slider/Type=31.png')] bg-no-repeat md:w-[302px] h-[162px] ">
+                            <div id='text-content' className=" flex justify-between w-[302px] h-[54px] px-[20px] py-[18vh]">
+                                <div className="text-[18px] font-bold">Don't Look Up</div>
+                                <div id="txt-rate-film"> &#9733; 4.5/5</div>
+                            </div>
                         </div>
-                    
                     </div>
-                </div>
 
-                <div id="img-all">
-                    <div id="txt-nama-film">All of us are dead
-                        <div id="txt-rate-film">&#9733;  4.2/5</div>
+                    <div id="img-all">
+                           <div id="txt-nama-film" className="flex text bg-[url('../../img/slider/Type=17.png')] bg-no-repeat md:w-[302px] h-[162px] ">
+                            <div id='text-content' className=" flex justify-between w-[302px] h-[54px] px-[20px] py-[18vh]">
+                                <div className="text-[18px] font-bold">All of us are dead</div>
+                                <div id="txt-rate-film"> &#9733; 4.2/5</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div id="img-blue">
-                    <div id="txt-nama-film">Blue Lock 
-                        <div id="txt-rate-film">&#9733; 4.6/5</div>
+                    <div id="img-blue">
+                        <div id="txt-nama-film" className="flex text bg-[url('../../img/slider/Type=5.png')] bg-no-repeat md:w-[302px] h-[162px] ">
+                            <div id='text-content' className=" flex justify-between w-[302px] h-[54px] px-[20px] py-[18vh]">
+                                <div className="text-[18px] font-bold">Blue Lock</div>
+                                <div id="txt-rate-film"> &#9733; 4.6/5</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div id="img-otto">
-                    <div id="btn-arrow-right-sld1" />
-                    <div id="txt-sld-terakhir">A Main Called Otto 
-                        <div id="txt-rate-film">&#9733; 4.4/5</div>
+                    <div id="img-otto">
+                           <div id="txt-nama-film" className="flex text bg-[url('../../img/slider/Type=9.png')] bg-no-repeat md:w-[302px] h-[162px] ">
+                            <div id='text-content' className=" flex justify-between w-[302px] h-[54px] px-[20px] py-[18vh]">
+                                <div className="text-[18px] font-bold">A Man Called Otto</div>
+                                <div id="txt-rate-film"> &#9733; 4.4/5</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </section>
-        
+            <div id="btn-arrow-left" className="bottom-[-38vh] left-[4vw] bg-[#2F3334] w-[44px] h-[44px] rounded-[24px] border-[1px] p-[10px] gap-[10px] absolute border-[#E7E3FC3B]">
+                <img className='w-[16px] h-[16px] absolute right-[12px] top-[13px]' src={arwLeft} alt="left" />
+            </div>
+            <div id="btn-arrow-right" className='bottom-[-38vh] right-[2vw] bg-[#2F3334] w-[44px] h-[44px] rounded-[24px] border-[1px] p-[10px] gap-[10px] absolute border-[#E7E3FC3B]'>
+        <img src={arwRight} alt="right" className='w-[16px] h-[16px] absolute right-[12px] top-[13px]' />
+            </div>
+
+               
+        </section>
         </>
 
     )
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,27 +4,29 @@ import dropdownAvatar from '../img/dropdown/icon1.png'
 import dropdownPremium from '../img/dropdown/icon2.png'
 import dropdownLogout from '../img/dropdown/icon3.png'
 import logo from '../img/logo-chill.png'
+import fullLogo from '../img/logo-full.png'
 
 const Navbar = () => {
 
     return(
         <>
-        <header className='bg-[#181A1C] flex w-[1440px] sm:w-[360px] max-w-[1440px] h-[94px] sm:h-[56px] px-[20px] py-[3vh]'>
-                <div className='w-[268px] h-[44px] flex gap-[12px]' >
-                    <img src={logo} alt="logo" className='w-4 h-4'  />
-                    <nav  className='flex gap-6 py-[1vh]'>
-                        <li className='list-none text-white w-[28px] h-[14px] text-[10px] font-medium'>Series</li>
-                        <li className='list-none text-white w-[20px] h-[14px] text-[10px] font-medium'>Film</li>
-                        <li className='list-none text-white w-[53px] h-[14px] text-[10px] font-medium'>Daftar Saya</li>
+        <header className='bg-[#181A1C] flex md:w-[1440px] sm:w-[360px] md:justify-between md:py-[25px] md:px-[80px]   md:h-[94px] sm:h-[56px] px-[20px] py-[3vh]'>
+                <div className='md:w-[1440px] md:text-4xl sm:text-[12px]  sm:w-[268px] md:h-[94px] sm:h-[44px] flex md:gap-[80px] sm:gap-[12px]' >
+                    <img src={fullLogo} alt="logo" className='md:block max-sm:-hidden md:w-[103.55px] md:h-[44px]  md:gap-[4px]'  />
+                    <img src={logo} alt="logo" className='md:hidden  sm:block sm:w-[20px] sm:h-[44px] gap-[4px]'  />
+                    <nav  className='flex md:gap-[80px] sm:gap-[12px] py-[1vh]'>
+                        <li className='list-none text-white md:w-[49px] sm:w-[28px] h-[14px] md:text-lg sm:text-[10px] font-medium'>Series</li>
+                        <li className='list-none text-white md:w-[35px] sm:w-[20px] h-[14px] md:text-lg sm:text-[10px] font-medium'>Film</li>
+                        <li className='list-none text-white md:w-[95px] sm:w-[53px] h-[14px] md:text-lg sm:text-[10px] font-medium'>Daftar Saya</li>
                     </nav>
                 </div>
                 <div class="setting-profile">
                     {/* <!-- new dropdown --> */}
                     <div class="profile-dropdown">
                         <div class="profile-dropdown">
-                            <div className='w-[40px] h-[20px] flex gap-[4px]'>
-                                <img className='w-[20px] h-[20px]' src={iconAvatar} alt="User Avatar"/>
-                                <img className='w-{16px} h-[16px]' src={iconDropDown} alt="dropdown"/>
+                            <div className='md:w-[76px] md:h-[40px] md:gap-[8px] sm:w-[40px] sm:h-[20px] flex sm:gap-[4px]'>
+                                <img className='md:w-[40px] md:h-[40px]  sm:w-[20px] sm:h-[20px]' src={iconAvatar} alt="User Avatar"/>
+                                <img id='btnDropDown' className='md:w-[28px] md:h-[28px] relative md:top-[10.02px] md:left-[7px]  sm:w-{16px} sm:h-[16px]' src={iconDropDown} alt="dropdown"/>
                             </div>
                         </div>
                     </div>
